f3c8fb039c4e3364710b51f5129e6e9b11355eff
Egor <meekgot@DESKTOP-1GSQ3A2.localdomain>
2020-05-20 22:28:42 +0300

    feat: scrolling func completed

---
+++ src/js/components/scrolling.js
@@ -0,0 +1,19 @@
+import animateScrollTo from 'animated-scroll-to';
+import _ from 'lodash';
+
+export default function Scrolling () {
+    const menuItems = document.querySelectorAll('.js-menu-item');
+
+    const scrollTo = (item) => {
+        const where = item.getAttribute('data-scroll-to');
+        const block = document.querySelector(`.${where}`);
+
+        animateScrollTo(block);
+    }
+
+    const bindItems = (item) => {
+        item.addEventListener('click', () => scrollTo(item))
+    }
+
+    _.each(menuItems, (item) => bindItems(item))
+}
